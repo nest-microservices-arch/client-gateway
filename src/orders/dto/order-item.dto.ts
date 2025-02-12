5ee63589f636e755775a9eb954da1005bcf20359
drdaza <drdaza18@gmail.com>
2025-02-12 22:45:23 +0100

    Refactor CreateOrderDto to use OrderItemDto for structured order creation
    - Replaced previous order properties with an array of OrderItemDto
    - Added validation for order items using class-validator decorators
    - Created new OrderItemDto to represent individual order line items
    - Updated index.ts to export the new OrderItemDto

---
+++ src/orders/dto/order-item.dto.ts
@@ -0,0 +1,17 @@
+import { IsPositive } from 'class-validator';
+
+import { IsNumber } from 'class-validator';
+
+export class OrderItemDto {
+  @IsNumber()
+  @IsPositive()
+  productId: number;
+
+  @IsNumber()
+  @IsPositive()
+  quantity: number;
+
+  @IsNumber()
+  @IsPositive()
+  price: number;
+}
